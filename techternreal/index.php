1b76aa503be08cdc183c636c99e8f7d132108b91
prinixtech <78962505+prinixtech@users.noreply.github.com>
2021-02-12 21:03:50 +0530

    Add files via upload

---
+++ techternreal/index.php
@@ -0,0 +1,459 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>www.techtern.com</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width , initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <!--fontawesom code-->
+ <link rel="stylesheet" type="text/css" href="css/all.min.css">
+ <!--googlefont code-->
+
+ <!--style code-->
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<!--Start Navigation-->
+<nav class="navbar navbar-expand-sm navbar-custom  navbar-dark fixed-top ">
+	<div class="container text-uppercase p-3">
+	<a href="index.php" class="navbar-brand logo "><img src="">Techtern</a>
+
+<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu" aria-controls="navbarNavAltMarkup" arai-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="myMenu">
+	<ul class="navbar-nav pl-5 custom-nav ">
+	<li class="nav-item  custom-nav-item "><a href="index.php" class="nav-link ">Home</a></li>	
+	<li class="nav-item  custom-nav-item"><a href="#services" class="nav-link">About US</a></li>	
+	<li class="nav-item  custom-nav-item"><a href="#registration" class="nav-link">Courses</a></li>	
+	<li class="nav-item custom-nav-item "><a href="user/loginpage.php" class="nav-link">Gallery</a></li> 
+	<li class="nav-item custom-nav-item"><a href="user/loginpage.php" class="nav-link">Registration</a></li>
+	<li class="nav-item custom-nav-item"><a href="#contactus" class="nav-link">Contact US</a></li>	
+	<li class="nav-item custom-nav-item"><a href="#contactus" class="nav-link">Feedback</a></li>	
+	</ul>
+</div>
+</div>
+</nav>
+<!--End Navigation-->
+
+
+<!--Strat Slider Section-->
+<div class="container-fluid remove-vid-marg ">
+	<div class="vid-parent">
+	<video playsinline autoplay muted loop>
+		<source src="video/banvid.mp4">
+	</video>
+	<div class="vid-overlay">	</div>	
+	</div>
+	<div class="vid-content">
+		<h1 class="my-content">Building the Future with Techtern</h1>
+		<p class="my-content">	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur.</p>
+		<br>
+		<br>
+		<a href="#" class="btn btn-danger button1">ContactUs</a>
+		</div>
+</div>
+<!--End Slider Section-->
+
+<!--Start Text Banner  Section-->
+<div class="container-fluid bg-dark  txt-banner">	
+
+    <div class="row bottom-banner">
+    	
+    	<div class="col-sm text-center  mt-4">	
+    	<h4><i class="fas fa-laptop-code mr-3 ">	</i><bold>"The Technoology works for tomorrow"</bold></h4>
+    	</div>
+    	<div class="col-sm text-center">	
+    		<h5><i class="fas fa-book-open mr-3">	</i> Register Now for 2021 TechTern Training  <br>	<a href="#" class="btn btn-danger button1">Register Here</a></h5>
+    		
+    	</div>
+
+	</div>
+</div>
+
+<!--End Text Banner  Section-->
+
+
+<!--Start Course Section-->
+
+<section class="section-1 mb-4" >
+	<div class="conatiner  text-center">
+		<h1 class="text-center font-weight-bold mt-4" >Meet Our Course area</h1>
+		<p  class="para-1 mt-3 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. 
+	</p>
+			</div>
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-4 clo-md-4 col-12">
+						<div class="card ho shadow  text-center " style="widht:20rem;">
+							<div class="inner view-overlay">
+							<img src="img/web_development.jpg" atl="web_development" class="card-img-top ">
+							<a href="#">
+							<div class="mask rgba-white-slight">	</div>
+						</a>
+					</div>
+						<div class="card-body ">
+      <h4 class="card-title">Web development</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+ 
+                 </div>
+                 	</div>
+					
+					<div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow text-center" style="widht:20rem;">
+								<div class="inner view-overlay">
+
+							<img src="img/web_desging.jpg" atl="web_development" class="card-img-top ">
+						</div>
+										<div class="card-body ">
+      <h4 class="card-title">Web designing</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+                     		</div>
+
+ 
+                 </div>
+                 <div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow text-center" style="widht:20rem;">
+								<div class="inner view-overlay">
+
+							<img src="img/ethical-hacking.jpg" atl="web_development" class="card-img-top">
+						</div>
+						<div class="card-body">
+      <h4 class="card-title">Ehical Hacking</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+                     </div>
+ 
+                 </div>
+                 <div class="row mt-4">
+					<div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow  text-center" style="widht:20rem;">
+								<div class="inner view-overlay">
+							<img src="img/wordpress.jpg" atl="web_development" class="card-img-top">
+						</div>
+						<div class="card-body  ">
+      <h4 class="card-title">Wordpress</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+ </div>	
+                 </div>
+					
+					<div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow text-center" style="widht:20rem;">
+								<div class="inner view-overlay">
+							<img src="img/adobexd.jpg" atl="web_development" class="card-img-top">
+						</div>
+						<div class="card-body ">
+      <h4 class="card-title">Adobe XD</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+ </div>
+                 </div>
+                 <div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow text-center" style="widht:20rem;">
+								<div class="inner view-overlay">
+							<img src="img/datascience.jpg" atl="web_development" class="card-img-top">
+						</div>
+						<div class="card-body">
+      <h4 class="card-title">Data Science</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+ </div>
+                 </div>
+				</div>
+			</div>
+</section>	
+
+<!--End Course Section-->
+
+
+
+<!--End Course 2 Section-->
+
+<section class="section-1 mb-4" >
+	<div class="conatiner  text-center">
+		<h1 class="text-center font-weight-bold mt-4" >Training Program</h1>
+		<p  class="para-1 mt-3 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. 
+	</p>
+			</div>
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow  text-center" style="widht:20rem;">
+							<div class="inner">
+							<img src="img/vocational_training.jpg" atl="web_development" class="card-img-top ">
+					</div>
+						<div class="card-body ">
+      <h4 class="card-title">Vocational Training</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+ 
+                 </div>
+                 	</div>
+					
+					<div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow text-center" style="widht:20rem;">
+								<div class="inner">
+
+							<img src="img/php.jpg" atl="web_development" class="card-img-top ">
+						</div>
+										<div class="card-body ">
+      <h4 class="card-title">Summer Training</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+                     		</div>
+
+ 
+                 </div>
+                 <div class="col-lg-4 clo-md-4 col-12">
+						<div class="card shadow text-center" style="widht:20rem;">
+								<div class="inner">
+
+							<img src="img/personality_development.jpg" atl="web_development" class="card-img-top">
+						</div>
+						<div class="card-body">
+      <h4 class="card-title">Personality Development & Special Training</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">Know More</a>
+                     </div>
+                     </div>
+ 
+                 </div>
+ 
+			</div>
+</section>	
+<!--End Course Section-->
+
+
+
+<!--Start Our services code-->
+<section class="serviceoffers" id="servicediv">
+	<div class="conatiner heading text-center">
+		<h1 class="text-center font-weight-bold" >What Do We Offer</h1>
+		<p class="text-center ">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.</p>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-10 offset-1 offset-lg-0 offset-md-0">
+				<div class="names my-3">
+					<h1>
+						HTML
+					</h1>
+					<div class="progress-bar progress-bar-striped progress-bar-animated" style="width:100%">100%</div>
+				</div>
+				<div class="names my-3">
+					<h1>
+						CSS
+					</h1>
+					<div class="progress-bar bg-danger progress-bar-striped  progress-bar-animated" style="width:60%">60%</div>
+				</div>
+				<div class="names my-3">
+					<h1>
+						JavaScript
+					</h1>
+					<div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" style="width:70%">70%</div>
+				</div>
+				<div class="names my-3">
+					<h1>
+						ReactJS
+					</h1>
+					<div class="progress-bar bg-info progress-bar-striped progress-bar-animated" style="width:50%">50%</div>
+				</div>
+
+				
+			</div>
+			<div class="col-lg-6 col-md-12 col-10 offset-1 offset-lg-0 offset-md-0">
+				<div class="row">
+					<div class="col-lg-2 col-2 service-icon">
+						<i class="fa fa-desktop fa-2x" aria-hidden="true" style="color: #002753"></i>
+					</div> 
+					<div class="col-lg-10 col-10 service-div">
+						<h2>Website Development</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div> 
+				</div>
+				<div class="row">
+					<div class="col-lg-2 col-2 service-icon">
+						<i class="fa fa-wifi fa-2x" aria-hidden="true" style="color: #002753"></i>
+					</div> 
+					<div class="col-lg-10 col-10 service-div">
+						<h2>Digital Marketing</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div> 
+				</div>
+				<div class="row">
+					<div class="col-lg-2 col-2 service-icon">
+						<i class="fa fa-phone fa-2x" aria-hidden="true" style="color: #002753"></i>
+					</div> 
+					<div class="col-lg-10 col-10 service-div">
+						<h2>Support 24/7</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div> 
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--End Our services code-->
+
+
+<!--Start Subscribe us code-->
+<section class="newsletter" id="newsletterid" >
+	<div class="conatiner heading text-center">
+		<h1 class="text-center font-weight-bold" >SUBSCRIBE TO OUR NEWSLETTER</h1>
+
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 offset-lg-2 col-12">
+				  <div class="input-group mb-3">
+    <input type="text" class="form-control news-input" placeholder="Your Email">
+    <div class="input-group-append">
+      <span class="input-group-text">SBSCRIBE</span>
+    </div>
+  </div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<!--End Subscribe us code-->
+
+
+
+<!--Start Footer code-->
+
+<footer class="footersection" id="footerdiv">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-6 col-12 footer-div">
+				<div>
+					<h3>About Techrern</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-12 footer-div text-center">
+				<div>
+					<h3>Navigation Link</h3>
+					<li><a href="#">HOME</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Proce</a></li>
+					<li><a href="#">About</a></li>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-12 footer-div">
+				<div>
+					<h3>NEWSLETTER</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+					<div class="container newsletter-main">
+		                  <div class="row">
+			                 <div class="col-lg-12 ">
+				                   <div class="input-group mb-3">
+                        <input type="text" class="form-control news-input" placeholder="Your Email">
+                       <div class="input-group-append">
+                              <span class="input-group-text">SBSCRIBE</span>
+                             </div>
+                            </div>
+			                </div>
+		                  </div>
+	                 </div>
+
+				</div>
+
+			</div>
+		</div>
+		<div class="mt-5 text-center">
+			<p>
+				copyright @ 2019 
+			</p>
+		</div>
+	</div>
+	<div class="scrolltop float-right">
+		<i class="fa fa-arrow-up" onclick="topfunction()" id="mybtn"></i>
+		
+
+	</div>
+</footer>
+
+<!--End Footer code-->
+
+<!--java Script File-->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/popper.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/all.min.js"></script>
+<script >
+	$(document).ready(function()
+	{
+		new WOW().init();
+	});
+</script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
